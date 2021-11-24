987c5231b18aa2c7c0ba4fd8e023210092d23239
malakivandas <malakivandas@gmail.com>
2021-11-24 13:42:47 -0800

    Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import { Fragment } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 
 //Components
-import Navbar from './components/Navbar';
+import Navbar from './components/layout/Navbar';
+import ReportsPage from './components/pages/ReportsPage';
+import SettingsPage from './components/pages/SettingsPage';
 
 function App() {
   return (
-    <Fragment>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ReportsPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+      </Routes>
       <Navbar />
-    </Fragment>
+    </Router>
   );
 }
 

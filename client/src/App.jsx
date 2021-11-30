baf468e8c260fbe6754fe77dbae70ad0e6aa56df
malakivandas <malakivandas@gmail.com>
2021-11-29 19:12:27 -0800

    Removed App.css as per PR3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import './App.css';
-
 //Components
 import Navbar from './components/layout/Navbar';
 import ReportsPage from './components/pages/ReportsPage';

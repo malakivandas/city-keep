2ebe74c4bd6c1f2dc3c77bcedfa420a3f1bda3df
malakivandas <malakivandas@gmail.com>
2022-01-05 12:28:50 -0800

    Database Constraints

--- server/models/database.sql
+++ server/models/database.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   last_name VARCHAR(50) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   city VARCHAR(50) NOT NULL,
   state_province VARCHAR(50) NOT NULL,
   country VARCHAR(50) NOT NULL
@@ -18,15 +18,17 @@ CREATE TABLE reports (
   user_id INT REFERENCES users (id) NOT NULL,
   category_id INT REFERENCES categories (id) NOT NULL,
   description TEXT NOT NULL,
-  r_time TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+  report_time TIMESTAMP(0) WITH TIME ZONE NOT NULL,
   address VARCHAR(255) NOT NULL,
   longitude DECIMAL(9,6) NOT NULL,
-  latitude DECIMAL(8,6) NOT NULL
+  latitude DECIMAL(8,6) NOT NULL,
+  UNIQUE (user_id, report_time)
 );
 
 CREATE TABLE images (
   report_id INT REFERENCES reports (id) NOT NULL,
-  image_url text NOT NULL
+  image_url text NOT NULL,
+  UNIQUE (report_id, image_url)
 );
 
 INSERT INTO users (last_name, first_name, email, city, state_province, country) VALUES ('Last', 'First', 'test@test.com', 'Vancouver', 'BC', 'Canada');
@@ -46,9 +48,9 @@ INSERT INTO categories (id, label) VALUES (12, 'Trees');
 INSERT INTO categories (id, label) VALUES (13, 'Utilities/Infrastructure');
 INSERT INTO categories (id, label) VALUES (14, 'Other');
 
-INSERT INTO reports (user_id, category_id, description, r_time, address, longitude, latitude) VALUES (1, 7, 'Test report 1. Category selected as "Signs".', '2003-04-12 04:05:06 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
-INSERT INTO reports (user_id, category_id, description, r_time, address, longitude, latitude) VALUES (1, 12, 'Test report 2. Category selected as "Trees".', '2003-04-12 04:05:06 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
-INSERT INTO reports (user_id, category_id, description, r_time, address, longitude, latitude) VALUES (1, 14, 'Test report 3. Category selected as "Other".', '2003-04-12 04:05:06 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
+INSERT INTO reports (user_id, category_id, description, report_time, address, longitude, latitude) VALUES (1, 7, 'Test report 1. Category selected as "Signs".', '2003-04-12 04:05:06 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
+INSERT INTO reports (user_id, category_id, description, report_time, address, longitude, latitude) VALUES (1, 12, 'Test report 2. Category selected as "Trees".', '2003-04-12 04:05:07 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
+INSERT INTO reports (user_id, category_id, description, report_time, address, longitude, latitude) VALUES (1, 14, 'Test report 3. Category selected as "Other".', '2003-04-12 04:05:08 PST', '0000 W Georgia St., Vancouver, BC, V0V 0V0', 100.100000, 30.300000);
 
 INSERT INTO images (report_id, image_url) VALUES (1, 'https://media-exp1.licdn.com/dms/image/C4D1BAQFFA6qBq7hHzg/company-background_10000/0/1622840496876?e=2159024400&v=beta&t=SDuw0kCp0yklzQhJkbARi0BkV8IcOxVVBwxGKOM3lD4');
 INSERT INTO images (report_id, image_url) VALUES (2, 'https://lp-cms-production.imgix.net/2021-04/GettyRF_612004360.jpg');

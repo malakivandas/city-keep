4f7944905641fd157e6a58571f30928f65536d39
malakivandas <malakivandas@gmail.com>
2022-01-10 13:53:10 -0800

    AuthOutlet and NoAuthOutlet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,20 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //Components
 import Navbar from './components/layout/Navbar';
 import Heading from './components/layout/Heading';
-import PrivateOutlet from './components/routing/PrivateOutlet';
+import AuthOutlet from './components/routing/AuthOutlet';
+import NoAuthOutlet from './components/routing/NoAuthOutlet';
 import ReportsPage from './components/pages/ReportsPage';
 import SettingsPage from './components/pages/SettingsPage';
 import LandingPage from './components/pages/LandingPage';
 
 function App() {
-  const isAuthenticated = true;
+  const isAuthenticated = false;
 
   return (
     <Router>
       <Heading />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route element={<PrivateOutlet isAuthenticated={isAuthenticated} />}>
+        <Route element={<NoAuthOutlet isAuthenticated={isAuthenticated} />}>
+          <Route path="/" element={<LandingPage />} />
+        </Route>
+        <Route element={<AuthOutlet isAuthenticated={isAuthenticated} />}>
           <Route path="reports" element={<ReportsPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>
---
+++ client/src/components/routing/NoAuthOutlet.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+
+export const NoAuthOutlet = ({ isAuthenticated }) => {
+  return !isAuthenticated ? <Outlet /> : <Navigate to="/reports" />;
+};
+
+export default NoAuthOutlet;

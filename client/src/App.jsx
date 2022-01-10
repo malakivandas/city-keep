426011d327a0a63fb6d475ebdeb89887805ef6dc
malakivandas <malakivandas@gmail.com>
2022-01-10 13:39:10 -0800

    Sign In and Sign Up tabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,8 +7,7 @@ import Heading from './components/layout/Heading';
 import PrivateOutlet from './components/routing/PrivateOutlet';
 import ReportsPage from './components/pages/ReportsPage';
 import SettingsPage from './components/pages/SettingsPage';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+import LandingPage from './components/pages/LandingPage';
 
 function App() {
   const isAuthenticated = true;
@@ -17,12 +16,8 @@ function App() {
     <Router>
       <Heading />
       <Routes>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route
-          path="/"
-          element={<PrivateOutlet isAuthenticated={isAuthenticated} />}
-        >
+        <Route path="/" element={<LandingPage />} />
+        <Route element={<PrivateOutlet isAuthenticated={isAuthenticated} />}>
           <Route path="reports" element={<ReportsPage />} />
           <Route path="settings" element={<SettingsPage />} />
         </Route>

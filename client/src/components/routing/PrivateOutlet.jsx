d8d060133c0045512a7fe68bbd68c1407d7ac85a
malakivandas <malakivandas@gmail.com>
2022-01-10 13:53:10 -0800

    AuthOutlet and NoAuthOutlet

--- client/src/components/routing/PrivateOutlet.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Navigate, Outlet } from 'react-router-dom';
-
-export const PrivateOutlet = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateOutlet;

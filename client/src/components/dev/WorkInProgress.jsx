c702f9eff9105bd70dfb2c9bf1c7243222d2ea81
braidencutforth <braiden89@gmail.com>
2022-02-07 20:01:02 -0700

    show

--- client/src/components/dev/WorkInProgress.jsx
+++ client/src/components/dev/WorkInProgress.jsx
@@ -2,43 +2,40 @@ import React, { useState } from 'react';
 
 import { Tooltip, ClickAwayListener } from '@mui/material';
 
-const WorkInProgress = ({
-  message,
-  placement,
-  sx,
-  children,
-}) => {
-  const [open, setOpen] = useState(false);
+const WorkInProgress = ({ message, placement, sx, children }) => {
+    const [open, setOpen] = useState(false);
+    const [coords, setCoords] = useState({ x: 0, y: 0 });
 
-  const handleTooltipClose = () => {
-    setOpen(false);
-  };
+    const handleTooltipClose = () => {
+        setOpen(false);
+    };
 
-  const handleTooltipOpen = () => {
-    setOpen(true);
-  };
+    const handleTooltipOpen = (e) => {
+        setCoords({ x: e.clientX, y: e.clientY });
+        setOpen(true);
+    };
 
-  return (
-    <ClickAwayListener onClickAway={handleTooltipClose}>
-      <Tooltip
-        title={message || 'Work in progress...'}
-        placement={placement}
-        arrow
-        PopperProps={{
-          disablePortal: true,
-        }}
-        onClose={handleTooltipClose}
-        onClick={handleTooltipOpen}
-        open={open}
-        disableFocusListener
-        disableHoverListener
-        disableTouchListener
-        sx={sx}
-      >
-        {children}
-      </Tooltip>
-    </ClickAwayListener>
-  );
+    return (
+        <ClickAwayListener onClickAway={handleTooltipClose}>
+            <Tooltip
+                title={message || 'Work in progress...'}
+                placement={placement}
+                arrow
+                PopperProps={{
+                    disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                onClick={handleTooltipOpen}
+                open={open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                sx={{ ...sx, left: coords.x, top: coords.y, position: 'fixed' }}
+            >
+                {children}
+            </Tooltip>
+        </ClickAwayListener>
+    );
 };
 
 export default WorkInProgress;
